c2252534e9da3953784ddf5366081031463fe479
Holly L Malm <holly.malm5@gmail.com>
2015-04-29 02:46:06 -0700

    cleaning up code

--- Levelup.py
+++ Levelup.py
@@ -162,4 +162,4 @@ def date_converstion(date):
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     # print "port: " + str(port)
-    app.run(debug=False, host="0.0.0.0", port=port)
+    app.run(debug="No_DEBUG", host="0.0.0.0", port=port)
